4a646c316b586b827dc8828e8fc9b1691b5a34ba
FernandoGM2701 <fernandogm2701@gmail.com>
2020-08-23 12:25:58 -0500

    Commit 23 de agosto de 2020

--- App.js
+++ App.js
@@ -1,14 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
+import MapView, {Marker, ProviderPropType} from 'react-native-maps' 
 
-export default function App() {
-  return (
+const {width, height} = Dimensions.get('window');
+
+const ASPECT_RATIO = width/height;
+const LATITUDE = -11.964988435826776;
+const LONGITUDE = -77.07672668451737;
+const LATITUDE_DELTA = 0.04;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+class Mapa extends React.Component {
+
+constructor(props){
+  super(props);
+
+  this.state = {
+    region:{
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA
+    }
+  }
+}  
+
+  render(){
+    return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+      provider = {this.props.provider}
+      style={styles.map}
+      scrollEnabled={true}
+      zoomEnabled={true}
+      pitchEnabled={true}
+      rotateEnabled={true}
+      initialRegion={this.state.region}
+      showsUserLocation={true}
+      followsUserLocation={true}
+      >
+         
+      </MapView>
     </View>
-  );
+    );
+  }
+}
+
+Mapa.propTypes = {
+  provider: ProviderPropType,visual
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +57,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map:{
+    height:'100%',
+    width: '100%'
+  }
 });
+
+export default Mapa
